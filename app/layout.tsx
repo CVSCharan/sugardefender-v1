86dab70bed516ec2385c1003b589a6f9fb2916b1
CVS CHARAN <charan.cvs@gmail.com>
2024-06-26 03:03:22 +0530

    Richads Head Script added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Head from "next/head";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,12 +17,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-        <script
-          src="https://richinfo.co/richpartners/in-page/js/richads-ob.js?pubid=929555&siteid=347784"
-          async
-        ></script>
-      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,18 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 import MainPage from "./components/MainPage/MainPage";
 import SecondPage from "./components/SecondPage/SecondPage";
 import FirstPage from "./components/FirstPage/FirstPage";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <Head>
+        <script
+          src="https://richinfo.co/richpartners/in-page/js/richads-ob.js?pubid=929555&siteid=347784"
+          async
+        ></script>
+      </Head>
       <MainPage />
       <SecondPage />
       <FirstPage />
