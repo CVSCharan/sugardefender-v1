4f8921e913764acb55b84e857b247d2de88c1049
CVS CHARAN <charan.cvs@gmail.com>
2024-06-26 03:09:58 +0530

    Richads Head Script removed

--- app/page.tsx
+++ app/page.tsx
@@ -2,19 +2,10 @@ import styles from "./page.module.css";
 import MainPage from "./components/MainPage/MainPage";
 import SecondPage from "./components/SecondPage/SecondPage";
 import FirstPage from "./components/FirstPage/FirstPage";
-import Head from "next/head";
-import Script from "next/script";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <Head>
-        <title>Sugar Defender</title>
-      </Head>
-      <Script
-        src="https://richinfo.co/richpartners/in-page/js/richads-ob.js?pubid=929555&siteid=347784"
-        strategy="afterInteractive"
-      />
       <MainPage />
       <SecondPage />
       <FirstPage />
